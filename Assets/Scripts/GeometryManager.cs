7a0ab76c5e831425505119749f42992c0187eb81
David Acosta <david.acosta.ing@gmail.com>
2025-09-04 15:35:21 -0500

    Takes an url with valid json and generates the mesh

--- Assets/Scripts/GeometryManager.cs
+++ Assets/Scripts/GeometryManager.cs
@@ -3,40 +3,69 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 using Newtonsoft.Json;
+using Unity.VisualScripting;
+using UnityEngine.UI;
 
 public class GeometryManager : MonoBehaviour
 {
     [SerializeField] private string url = "https://api.jsonbin.io/v3/b/68b7786043b1c97be934ead9";
-    [SerializeField] private Material material;
+    [SerializeField] private Material opaqueMaterial;
+    [SerializeField] private Material transluscentMaterial;
     
     private JsonTemplate _jsonTemplate = new JsonTemplate();
+    private List<GameObject> _generatedObjects = new List<GameObject>();
+
     private void Start()
     {
-        NetworkManager.Instance.Get(url, ParseJson);
-        // Parse JsonFile
-        
-        
+        ViewControllerWindow.Instance.OnDropdown += Dropdown;
     }
 
-    private void ParseJson(string text)
+    private void OnDestroy()
     {
-        Debug.Log($"I got this from URL: {text}");
-        _jsonTemplate = JsonConvert.DeserializeObject<JsonTemplate>(text);
-        ViewControllerWindow.Instance.SetInfoText(JsonConvert.SerializeObject(_jsonTemplate));
+        ViewControllerWindow.Instance.OnDropdown -= Dropdown;
+    }
+
+    private void Dropdown(string value)
+    {
+        switch (value)
+        {
+            case "Display all":
+                SetAllOpaque();
+                break;
+            case "Translucent all":
+                SetAllTransluscent();
+                break;
+            default:
+                SetAllTransluscent();
+                SetVisible(value);
+                break;
+        }
+    }
+
+    public void GenerateFromURL(string jsonURL)
+    {
+        NetworkManager.Instance.Get(jsonURL, ParseJson);
+    }
+    private void ParseJson(string jsonString)
+    {
+        
+        Debug.Log($"I got this from URL: {jsonString}");
+        _jsonTemplate = JsonConvert.DeserializeObject<JsonTemplate>(jsonString);
         Debug.Log($"Json is getting parsed as {_jsonTemplate.record == null}");
-        var holderGameObject = new GameObject("MeshHolder");
+        var holderGameObject = new GameObject(_jsonTemplate.record.name);
         holderGameObject.AddComponent<MeshFilter>();
         holderGameObject.AddComponent<MeshRenderer>();
-        holderGameObject.GetComponent<MeshRenderer>().material = material;
+        holderGameObject.GetComponent<MeshRenderer>().material = opaqueMaterial;
         holderGameObject.transform.SetParent(transform);
         var mesh = CreateMesh();
-        mesh.name = "Generated mesh";
+        mesh.name = _jsonTemplate.record.name;
         holderGameObject.GetComponent<MeshFilter>().mesh = mesh;
+        _generatedObjects.Add(holderGameObject);
+        ViewControllerWindow.Instance.UpdateDropdown(holderGameObject);
     }
 
     private Mesh CreateMesh()
     {
-       Debug.Log("Creating mesh");
         var model3DJson = _jsonTemplate.record;
         Mesh mesh = new Mesh();
         mesh.vertices = new Vector3[model3DJson.vertices.Length];
@@ -60,11 +89,32 @@ public class GeometryManager : MonoBehaviour
                 triangles.Add(face[i + 1]);
             }
         }
-        // var vertex1 = new Vector3(model3DJson.vertices[0][1], model3DJson.vertices[0][1], model3DJson.vertices[0][2]);
-        // var vertex2 = new Vector3(model3DJson.vertices[1][1], model3DJson.vertices[1][1], model3DJson.vertices[1][2]);
-        // var vertex3 = new Vector3(model3DJson.vertices[2][1], model3DJson.vertices[2][1], model3DJson.vertices[2][2]);
-        // mesh.triangles =  new int[]{0, 1, 2, 0, 2, 3, 4, 5, 6, 4, 6, 7, 0, 1, 5, 0, 5, 4};
         mesh.triangles = triangles.ToArray();
         return mesh;
+    }
+
+    public void SetVisible(string targetObjectName)
+    {
+        _generatedObjects.Find(x => x.gameObject.name == targetObjectName).GetComponent<MeshRenderer>().material = opaqueMaterial;
+    }
+
+    public void SetTransluscent(string targetObjectName)
+    {
+        _generatedObjects.Find(x => x.gameObject.name == targetObjectName).GetComponent<MeshRenderer>().material = transluscentMaterial;
+    }
+
+    public void SetAllTransluscent()
+    {
+        foreach (var generatedObject in _generatedObjects)
+        {
+            generatedObject.GetComponent<MeshRenderer>().material = transluscentMaterial;
+        }
+    }
+    public void SetAllOpaque()
+    {
+        foreach (var generatedObject in _generatedObjects)
+        {
+            generatedObject.GetComponent<MeshRenderer>().material = opaqueMaterial;
+        }
     }
 }
--- Assets/Scripts/UI/ViewControllerWindow.cs
+++ Assets/Scripts/UI/ViewControllerWindow.cs
@@ -1,14 +1,22 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.UIElements;
+using Button = UnityEngine.UIElements.Button;
 
 public class ViewControllerWindow : MonoBehaviour
 {
     public static ViewControllerWindow Instance;
+    public event Action<String> OnDropdown;
 
+    [SerializeField] private GeometryManager geometryManager;
 
     private VisualElement _root;
     private Label _debugText;
+    private Button _generateButton;
+    private DropdownField _dropdownField;
+    private TextField _inputField;
     private void Awake()
     {
         if (Instance != null) Destroy(gameObject);
@@ -19,10 +27,38 @@ public class ViewControllerWindow : MonoBehaviour
     {
         _root = GetComponent<UIDocument>().rootVisualElement;
         _debugText = _root.Q<Label>("DebugLabelText");
+        _generateButton = _root.Q<Button>("GenerateButton");
+        _dropdownField =  _root.Q<DropdownField>("DropDown");
+        Debug.Log($"IsDropDown null ? {_dropdownField == null}");
+        _inputField = _root.Q<TextField>("URLField");
+        _dropdownField.choices = new List<string>();
+        _dropdownField.choices.Add("Display all");
+        _dropdownField.choices.Add("Translucent all");
+        _dropdownField.RegisterValueChangedCallback((evt) => OnDropdown?.Invoke(evt.newValue));
+        _generateButton.clicked += OnGenerateButtonClicked;
+    }
+
+    
+
+    private void OnDestroy()
+    {
+        _generateButton.clicked -= OnGenerateButtonClicked;
+        _dropdownField.UnregisterValueChangedCallback((evt) => OnDropdown?.Invoke(evt.newValue));
+    }
+
+    private void OnGenerateButtonClicked()
+    {
+        
+        geometryManager.GenerateFromURL(_inputField.value);
     }
 
     public void SetInfoText(string text)
     {
         _debugText.text = text;
+    }
+
+    public void UpdateDropdown(GameObject gameObject)
+    {
+        _dropdownField.choices.Add(gameObject.name);
     }
 }
